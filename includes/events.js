cd10434522cd749ffbb6fd399e9619526022201a
spyang302 <spyang302@yahoo.com>
2017-04-08 12:54:21 -0400

    Update events.js

--- includes/events.js
+++ includes/events.js
@@ -20,7 +20,6 @@ var idOp2;
 // Core event functions
 
 function genIdEvt() {
-	while(idEvt < countEvt)
 		idEvt = Math.floor(Math.random() * countEvt);
 }
 
@@ -28,6 +27,8 @@ function updateTxtEvt() {
 		if(idEvt == 1){
 			document.getElementById("evtText").value = "Commoner rebel against you due to extreme weather hurting crop yield";
 		}
+		handleBtnEvt1();
+		handleBtnEvt2();
 }
 ////////////////////////////////////////////////////////////////////////////////
 // Button handlers
@@ -35,26 +36,26 @@ function updateTxtEvt() {
 function handleBtnEvt1() {
 	if(idEvt == 1){
 		idOp1 = Math.floor(Math.random() * 6)
-		if(idOp == 1){
+		if(idOp1 == 1){
 			//Commoner Opinion drop by 10%
 			approvalCommon -= .10;
 			document.getElementById("btnEvt1").value = "Let them eat cake!";
 		}
-		else if(id Op == 2){
+		else if(idOp1 == 2){
 			//Commoner Opinion drop by 5$
 			approvalCommon -= .05;
 			document.getElementById("btnEvt1").value = "Send out gendarmerie! Supress this insurrection!";
 		}
-		else if(id Op == 3){
+		else if(idOp1 == 3){
 			//Commoner Opinion drop by 5$
 			approvalCommon -= .05;
 			document.getElementById("btnEvt1").value = "Silence them by giving a morale boosting speech";
 		}
-		else if(id Op == 4){
+		else if(idOp1 == 4){
 			//Neutral Option
 			document.getElementById("btnEvt1").value = "Partially subsidize the price of bread for the people";
 		}
-		else if(id Op == 5){
+		else if(idOp1 == 5){
 			//Commoner Opinion go up by 5%
 			approvalCommon += .05;
 			document.getElementById("btnEvt1").value = "Go to the food storage and hand out food to the citizens";
@@ -67,8 +68,32 @@ function handleBtnEvt2() {
 	while(idOp2 == idOp1){	
 		idOp2 = Math.floor(Math.random() * 6)
 	}
-	switch(idEvt) {
-		default:
-		break;
+	
+	if(idEvt == 1){
+		idOp2 = Math.floor(Math.random() * 6)
+		if(idOp2 == 1){
+			//Commoner Opinion drop by 10%
+			approvalCommon -= .10;
+			document.getElementById("btnEvt1").value = "Let them eat cake!";
+		}
+		else if(idOp2 == 2){
+			//Commoner Opinion drop by 5$
+			approvalCommon -= .05;
+			document.getElementById("btnEvt1").value = "Send out gendarmerie! Supress this insurrection!";
+		}
+		else if(idOp2 == 3){
+			//Commoner Opinion drop by 5$
+			approvalCommon -= .05;
+			document.getElementById("btnEvt1").value = "Silence them by giving a morale boosting speech";
+		}
+		else if(idOp2 == 4){
+			//Neutral Option
+			document.getElementById("btnEvt1").value = "Partially subsidize the price of bread for the people";
+		}
+		else if(idOp2 == 5){
+			//Commoner Opinion go up by 5%
+			approvalCommon += .05;
+			document.getElementById("btnEvt1").value = "Go to the food storage and hand out food to the citizens";
+		}
 	}
 }
