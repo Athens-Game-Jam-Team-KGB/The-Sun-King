24b859fd22c6beb45a2d27dc57c76162ab48ab71
RobertM14 <rwmaerz@gmail.com>
2017-04-08 13:34:16 -0400

    Update actions.js

--- includes/actions.js
+++ includes/actions.js
@@ -2,14 +2,17 @@
 // Slider handlers
 
 function handleSlideTaxPercentNobles() {
+	taxPercentNobles = Document.getElementById("slideTaxPercentNobles").value / 100;
 	updateControls();
 }
 
 function handleSlideTaxPercentClergy() {
+	taxPercentClergy = Document.getElementById("slideTaxPercentClergy").value / 100;
 	updateControls();
 }
 
 function handleSlideTaxPercentCommon() {
+	taxPercentCommon = Document.getElementById("slideTaxPercentCommon").value / 100;
 	updateControls();
 }
 
