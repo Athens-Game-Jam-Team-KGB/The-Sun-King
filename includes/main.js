411ded2261714445e6b6efcc281d2085f2dc3bd3
Miles B Huff <MilesBHuff@UGA.edu>
2017-04-07 21:37:11 -0400

    Update and rename include/main.js to includes/main.js

---
+++ includes/main.js
@@ -0,0 +1,34 @@
+var idEvt;
+var countEvt;
+var approvalClergy;
+var approvalNoble;
+var approvalCommon;
+
+function init() {
+	idEvt = Math.round(Math.random() * countEvt);
+}
+
+function turn() {
+	
+}
+
+function updateEvt() {
+	switch(idEvt) {
+		default:
+		break;
+	}
+}
+
+function evtOpt1() {
+	switch(idEvt) {
+		default:
+		break;
+	}
+}
+
+function evtOpt2() {
+	switch(idEvt) {
+		default:
+		break;
+	}
+}
