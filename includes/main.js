2a86f70f01b55360854a32c3873215f47eaeb7f8
Miles B Huff <MilesBHuff@UGA.edu>
2017-04-08 12:08:27 -0400

    Update main.js

--- includes/main.js
+++ includes/main.js
@@ -1,13 +1,61 @@
+// Parliamentary variables
 var   approvalClergy;
 var   approvalCommon;
-var   approvalNoble;
+var   approvalNobles;
+
+// Event variables
 const countEvt = 1;
 var   idEvt;
 
+// Economic variables
+var   estatePopulation;
+var   netChange;
+var   taxAmountNobles;
+var   taxAmountClergy;
+var   taxAmountCommon;
+var   taxPercentNobles;
+var   taxPercentClergy;
+var   taxPercentCommon;
+var   treasuryCurrent;
+var   treasuryFuture;
+const corruptPercentNobles = 1.0;
+const corruptPercentClergy = 0.7;
+const corruptPercentCommon = 0.3;
+var   outlayCorrupt;
+const outlayInterestPerLoan = 0;
+var   outlayInterestTotal;
+
+////////////////////////////////////////////////////////////////////////////////
+// Main functions
+
 function init() {
-	approvalClergy = 0.5;
-	approvalCommon = 0.5;
-	approvalNoble  = 0.5;
+	// Parliamentary variables
+	approvalClergy  = 0.5;
+	approvalCommon  = 0.5;
+	approvalNobles  = 0.5;
+	
+	// Event variables
+	idEvt = 0;
+	
+	// Economic variables
+	estatePopulation = 21000000 / 3; // We're pretending each state has the same population count in order to simulate some estates having more money than others
+	netChange        = 0;
+	outlayCorrupt    = 0;
+	outlayEvt        = 0;
+	outlayInterest   = 0;
+	outlayTotal      = 0;
+	receiptTotal     = 0;
+	taxAmountNobles  = 0;
+	taxAmountClergy  = 0;
+	taxAmountCommon  = 0;
+	taxPercentNobles = 0.1;
+	taxPercentClergy = 0.1;
+	taxPercentCommon = 0.1;
+	taxAmountTotal   = 0;
+	treasuryCurrent  = 100000;
+	treasuryFuture   = 0;
+	
+	// Start the first turn
 	turn();
 }
 
@@ -15,6 +63,9 @@ function turn() {
 	genIdEvt();
 	updateTxtEvt();
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// Helper functions
 
 function genIdEvt() {
 	while(idEvt < countEvt)
@@ -28,6 +79,9 @@ function updateTxtEvt() {
 	}
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// Button handlers
+
 function handleBtnEvt1() {
 	switch(idEvt) {
 		default:
@@ -40,4 +94,39 @@ function handleBtnEvt2() {
 		default:
 		break;
 	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Slider handlers
+
+function handleSlideTaxNobles() {
+	switch(idEvt) {
+		default:
+		break;
+	}
+}
+
+function handleSlideTaxClergy() {
+	switch(idEvt) {
+		default:
+		break;
+	}
+}
+
+function handleSlideTaxCommon() {
+	switch(idEvt) {
+		default:
+		break;
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Loans
+
+function loanTake() {
+	
+}
+
+function loanRepay() {
+	
 }
