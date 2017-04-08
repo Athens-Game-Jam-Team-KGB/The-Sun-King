560b8d631d9a5cfce78fb9e0586da53108e35c1f
Miles B Huff <MilesBHuff@UGA.edu>
2017-04-08 12:12:04 -0400

    Create input.js

---
+++ includes/input.js
@@ -0,0 +1,51 @@
+////////////////////////////////////////////////////////////////////////////////
+// Button handlers
+
+function handleBtnEvt1() {
+	switch(idEvt) {
+		default:
+		break;
+	}
+}
+
+function handleBtnEvt2() {
+	switch(idEvt) {
+		default:
+		break;
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Slider handlers
+
+function handleSlideTaxPercentNobles() {
+	switch(idEvt) {
+		default:
+		break;
+	}
+}
+
+function handleSlideTaxPercentClergy() {
+	switch(idEvt) {
+		default:
+		break;
+	}
+}
+
+function handleSlideTaxPercentCommon() {
+	switch(idEvt) {
+		default:
+		break;
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Loans
+
+function loanTake() {
+	
+}
+
+function loanRepay() {
+	
+}
