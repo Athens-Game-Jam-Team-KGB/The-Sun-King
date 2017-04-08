b2d030d5a4b7e6a9dc9ddc07f2b6752bd2527eaf
Miles B Huff <MilesBHuff@UGA.edu>
2017-04-08 12:14:05 -0400

    Update events.js

--- includes/events.js
+++ includes/events.js
@@ -1,9 +1,29 @@
+////////////////////////////////////////////////////////////////////////////////
+// Core event functions
+
 function genIdEvt() {
 	while(idEvt < countEvt)
 		idEvt = Math.floor(Math.random() * countEvt);
 }
 
 function updateTxtEvt() {
+	switch(idEvt) {
+		default:
+		break;
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Button handlers
+
+function handleBtnEvt1() {
+	switch(idEvt) {
+		default:
+		break;
+	}
+}
+
+function handleBtnEvt2() {
 	switch(idEvt) {
 		default:
 		break;
