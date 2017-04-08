2a5446d23685b338b04f0419a669befabca57e69
spyang302 <spyang302@yahoo.com>
2017-04-08 12:44:50 -0400

    Update events.js

--- includes/events.js
+++ includes/events.js
@@ -1,3 +1,21 @@
+var approvalClergy;
+var approvalCommon;
+var approvalNoble;
+var countEvt = 2;
+var idEvt
+var idOp1;
+var idOp2;
+
+/*function init() {
+	genIdEvt();
+	
+	turn();
+}*/
+
+/*function turn() {
+	genIdEvt();
+}*/
+
 ////////////////////////////////////////////////////////////////////////////////
 // Core event functions
 
@@ -7,23 +25,48 @@ function genIdEvt() {
 }
 
 function updateTxtEvt() {
-	switch(idEvt) {
-		default:
-		break;
-	}
+		if(idEvt == 1){
+			document.getElementById("evtText").value = "Commoner rebel against you due to extreme weather hurting crop yield";
+		}
 }
-
 ////////////////////////////////////////////////////////////////////////////////
 // Button handlers
 
 function handleBtnEvt1() {
-	switch(idEvt) {
-		default:
-		break;
+	if(idEvt == 1){
+		idOp1 = Math.floor(Math.random() * 6)
+		if(idOp == 1){
+			//Commoner Opinion drop by 10%
+			approvalCommon -= .10;
+			document.getElementById("btnEvt1").value = "Let them eat cake!";
+		}
+		else if(id Op == 2){
+			//Commoner Opinion drop by 5$
+			approvalCommon -= .05;
+			document.getElementById("btnEvt1").value = "Send out gendarmerie! Supress this insurrection!";
+		}
+		else if(id Op == 3){
+			//Commoner Opinion drop by 5$
+			approvalCommon -= .05;
+			document.getElementById("btnEvt1").value = "Silence them by giving a morale boosting speech";
+		}
+		else if(id Op == 4){
+			//Neutral Option
+			document.getElementById("btnEvt1").value = "Partially subsidize the price of bread for the people";
+		}
+		else if(id Op == 5){
+			//Commoner Opinion go up by 5%
+			approvalCommon += .05;
+			document.getElementById("btnEvt1").value = "Go to the food storage and hand out food to the citizens";
+		}
 	}
 }
 
 function handleBtnEvt2() {
+	idOp2 = Math.floor(Math.random() * 6)
+	while(idOp2 == idOp1){	
+		idOp2 = Math.floor(Math.random() * 6)
+	}
 	switch(idEvt) {
 		default:
 		break;
